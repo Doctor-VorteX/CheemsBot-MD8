bd07d7d795dad99ff8fde9b9f456f8fbbd778004
Ace <bambalelezothani1@gmail.com>
2024-10-08 18:45:16 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const chalk = require("chalk")
 const fs = require("fs")
 
 //aumto presence update
-global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoTyping = true //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
 global.autoblockmorroco = false //auto block 212 (true to on, false to off)
 global.autokickmorroco = false //auto kick 212 (true to on, false to off) 
@@ -21,27 +21,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['27682833105'] //ur owner number
+global.ownernomer = "27766547202" //ur owner number2
+global.ownername = "♡🌹President🌹♡" //ur owner name
+global.ytname = "YT: Ace🥂" //ur yt chanel name
+global.socialm = "GitHub: private" //ur github or insta name
+global.location = "Africa" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
-global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
-global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
+global.botname = "Shadow🌹Knight"
+global.ownernumber = '27766547202'
+global.ownername = '⚠️Ace'
+global.ownerNumber = ["27682833105@s.whatsapp.net"]
+global.ownerweb = "https://youtube.com/@faithfulflix-y3l?si=WweQNRmrSZEz6WUy"
+global.instagram = "https://www.instagram.com/x.the.motivator/profilecard/?igsh=ZHZtcG4waGlndDZ0"
+global.wagc = "https://whatsapp.com/channel/0029VaeZQRw0wajmH2N2sN17"
+global.themeemoji = '🥂'
+global.wm = "Shadow Bot Inc."
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "Ace🥂\n\n"
+global.creator = "27766547202@s.whatsapp.net"
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
